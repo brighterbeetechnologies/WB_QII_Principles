8ef7e9cf1693aacd314ed9d43078558d0227c0b6
tejaldalavi <hasetejal05@gmail.com>
2026-01-08 16:30:32 +0530

    links updated

--- src/pages/ManagingCyberSecurityRisks.jsx
+++ src/pages/ManagingCyberSecurityRisks.jsx
@@ -32,24 +32,24 @@ export default function ManagingCyberSecurityRisks() {
                     <NumSteps num="1"><strong>Building a foundation for cybersecurity: </strong>  This includes assessing cybersecurity maturity, developing strategies and legal frameworks, establishing cybersecurity governance structures, and strengthening institutions for implementation.</NumSteps>
                     
                     <div className="VCard-cnt col3">
-                        <VCard image="images/qii4/FinancingResilience/card1.png" title={<p><strong>Cybersecurity Assessment Toolkit for Smart Cities</strong></p>} buttonText="Tool" link="" >
+                        <VCard image="images/qii4/ManagingCyberSecurityRisks/card1.png" title={<p><strong>Cybersecurity Assessment Toolkit for Smart Cities</strong></p>} buttonText="Guidance" link="Cybersecurity Assessment Toolkit for Smart Cities" >
                             <p>The Cybersecurity Assessment Toolkit guides countries and sectors to assess, strengthen, and mainstream cyber resilience in critical infrastructure.</p>
                         </VCard>
-                
-                        <VCard image="images/qii4/FinancingResilience/card2.png" title={<p><strong>UK: The Cyber Assessment Framework </strong>(CAF) (National Cyber Security Center, UK)</p>} buttonText="Guidance" link="" >
+
+                        <VCard image="images/qii4/ManagingCyberSecurityRisks/card2.png" title={<p><strong>UK: The Cyber Assessment Framework </strong>(CAF) (National Cyber Security Center, UK)</p>} buttonText="Guidance" link="https://www.ncsc.gov.uk/collection/cyber-assessment-framework/introduction-to-caf#section_1" >
                             <p>This Cyber Assessment Framework (CAF) provides a systematic approach to assessing cyber risks and how they are being managed by the UK National Cyber Security Center.</p>
                         </VCard>
 
-                        <VCard image="images/qii4/FinancingResilience/card3.png" title={<p><strong>Sectoral Cybersecurity Maturity Model </strong>(World Bank)</p>} buttonText="Guidance" link="" >
+                        <VCard image="images/qii4/ManagingCyberSecurityRisks/card3.png" title={<p><strong>Sectoral Cybersecurity Maturity Model </strong>(World Bank)</p>} buttonText="Guidance" link="https://documents1.worldbank.org/curated/en/099062623085028392/pdf/P17263707c36b702309f7303dbb7266e1cf.pdf" >
                             <p>The Sectoral Cybersecurity Maturity Model evaluates cybersecurity maturity and provides actionable recommendations to strengthen cyber resilience, address vulnerabilities, and guide investment and policy decisions.</p>
                         </VCard>
                     </div>
                     <div className="VCard-cnt col2">
-                        <VCard image="images/qii4/FinancingResilience/card4.png" title={<p><strong>Cybersecurity Economics for Emerging Markets </strong>(World Bank)</p>} buttonText="Report" link="" >
+                        <VCard image="images/qii4/ManagingCyberSecurityRisks/card4.png" title={<p><strong>Cybersecurity Economics for Emerging Markets </strong>(World Bank)</p>} buttonText="Report" link="https://documentsinternal.worldbank.org/Search/34392134" >
                             <p>This report explores global cybersecurity threats and their impacts; the market failures that hinder responses; and proposes strategies, policies, and governance efforts to foster innovation and sustainability amid change and uncertainty. See page 87 onwards for a specific discussion on infrastructure.</p>
                         </VCard>
-                
-                        <VCard image="images/qii4/FinancingResilience/card5.png" title={<p><strong>Ghana: A case study in strengthening cyber resilience </strong>(World Bank)</p>} buttonText="Case study" link="" >
+
+                        <VCard image="images/qii4/ManagingCyberSecurityRisks/card5.png" title={<p><strong>Ghana: A case study in strengthening cyber resilience </strong>(World Bank)</p>} buttonText="Case study" link="https://documents1.worldbank.org/curated/en/099111623162584046/pdf/P17785201f69be0150909902c3a7202107e.pdf" >
                             <p>The case of Ghana demonstrates that forward-looking investments and policy initiatives based on international best-practices can go a long way in boosting cybersecurity capacity in developing countries.</p>
                         </VCard>
                     </div>
@@ -58,11 +58,11 @@ export default function ManagingCyberSecurityRisks() {
                     <NumSteps num="2"><strong>Implementing cyber security in practice: </strong>  This involves evaluating critical cybersecurity risks and putting practical measures in place to manage and lower these risks.</NumSteps>
 
                     <div className="VCard-cnt col2">
-                        <VCard image="images/qii4/FinancingResilience/card4.png" title={<p><strong>Cybersecurity best practice for smart cities </strong>(CISA)</p>} buttonText="Guidance" link="" >
+                        <VCard image="images/qii4/ManagingCyberSecurityRisks/card4.png" title={<p><strong>Cybersecurity best practice for smart cities </strong>(CISA)</p>} buttonText="Guidance" link="https://www.cisa.gov/sites/default/files/2023-04/cybersecurity-best-practices-for-smart-cities_508.pdf" >
                             <p>This document aims to guide the design, implementation, and use of studies to value the benefits and costs of Nature-Based Solutions (NBS) for climate resilience projects.</p>
                         </VCard>
                 
-                        <VCard image="images/qii4/FinancingResilience/card5.png" title={<p><strong>Canada: the cyber threat environment</strong></p>} buttonText="Guidance" link="" >
+                        <VCard image="images/qii4/ManagingCyberSecurityRisks/card5.png" title={<p><strong>Canada: the cyber threat environment</strong></p>} buttonText="Guidance" link="https://www.cyber.gc.ca/en/guidance/introduction-cyber-threat-environment" >
                             <p>Cost-effectiveness analysis and cost-benefit analysis of DRM projects have been widely implemented both at national and local levels in Japan. This resource covers the different procedures for such analysis, according to the type of project, the funds, and the governing entity responsible particularly in parts V and VI.</p>
                         </VCard>
                     </div>
