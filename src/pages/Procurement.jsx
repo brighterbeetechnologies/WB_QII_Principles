baa064e2e5ffc40c9f804d3831e32d57c2ae2e05
tejaldalavi <hasetejal05@gmail.com>
2026-03-16 22:24:55 +0530

    Update Procurement.jsx

--- src/pages/Procurement.jsx
+++ src/pages/Procurement.jsx
@@ -477,9 +477,23 @@ export default function Procurement() {
                       {/* <div className="page-resource-description">
                         {p.description}
                       </div> */}
-                      <Popover
+                      {/* <Popover
                         content={p.description}
                         // title={p.title}
+                        trigger="click"
+                      >
+                        <div className="page-resource-description">
+                          Read More...
+                        </div>
+                      </Popover> */}
+                      <Popover
+                        content={
+                          <div className="resource-popover-content">
+                            {p.description}
+                          </div>
+                        }
+                        placement="left"
+                        title={false}
                         trigger="click"
                       >
                         <div className="page-resource-description">
